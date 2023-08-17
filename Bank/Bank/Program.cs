7aa9d92beb7567d9b2e72483515cedc8f2e5475f
Dũng <114909128+dangtiendung1409@users.noreply.github.com>
2023-08-17 15:20:01 +0700

    Add files via upload

---
+++ Bank/Bank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Bank.demo1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BankAccount account = new BankAccount(1000);
+
+        account.BalanceChanged += Account_BalanceChanged;
+
+        Console.WriteLine($"Số dư hiện tại: {account.Balance}");
+
+        account.Deposit(500);
+        account.Withdraw(200);
+
+        Console.WriteLine($"Số dư cập nhật: {account.Balance}");
+    }
+
+    private static void Account_BalanceChanged(object sender, EventArgs e)
+    {
+        Console.WriteLine("Số dư đã được thay đổi");
+    }
+}
---
+++ Bank/Bank/demo1/BankAccount.cs
@@ -0,0 +1,66 @@
+﻿using System;
+namespace Bank.demo1;
+
+
+class BankAccount
+{
+    private decimal balance; 
+
+    public decimal Balance
+    {
+        get { return balance; }
+        private set
+        {
+            if (balance != value)
+            {
+                balance = value;
+                OnBalanceChanged();
+            }
+        }
+    }
+
+    public event EventHandler BalanceChanged;
+
+    protected virtual void OnBalanceChanged()
+    {
+       
+        BalanceChanged?.Invoke(this, EventArgs.Empty); 
+    }
+
+    public BankAccount(decimal initialBalance)
+    {
+        if (initialBalance < 0)
+        {
+            throw new ArgumentException("Initial balance cannot be negative.");
+        }
+
+        Balance = initialBalance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentException("Deposit amount must be greater than zero.");
+        }
+
+        Balance += amount;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentException("Withdrawal amount must be greater than zero.");
+        }
+
+        if (amount > Balance)
+        {
+            throw new InvalidOperationException("Insufficient funds.");
+        }
+
+        Balance -= amount;
+    }
+}
+
+
